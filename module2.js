bef9c0f5032c8de048cb799b0e84910835a73c5b
margareta75 <margareta.xx@gmail.com>
2020-09-04 22:27:37 +0200

    fixed comments

--- module2.js
+++ module2.js
@@ -1,6 +1,11 @@
 (function () {
+  // global variables
   const sentenceButton = document.querySelector(".sentenceButton");
+  let choosedSentence = "";
+  let choosedColor = "";
+  let condition = false;
 
+  // array with sentences
   const sentenceList = [
     "Keep your eyes on the stars and your feet on the ground. Theodore Roosevelt",
     "It’s better to be a lion for a day than a sheep all your life. Elizabeth Kenny",
@@ -14,6 +19,7 @@
     "Be happy for this moment. This moment is your life. Omar Khayyam",
   ];
 
+  // array with colors
   const rainbow = [
     "red",
     "orange",
@@ -24,24 +30,25 @@
     "violet",
   ];
 
-  let choosedSentence = "";
-  let choosedColor = "";
-  let condition = false;
-
+  // function to run if condition is true
   const addTheBestText = () => {
     document.querySelector("#text2").innerText =
       "This is the best quote of the day ever!";
   };
 
+  // function to choose quote of the day and change color of header
   const chooseSentenceAndChangeColor = () => {
+    // loop to choose quote
     let i;
     for (i = 0; i < sentenceList.length; i++) {
       choosedSentence = sentenceList[Math.floor(10 * Math.random())];
     }
+    // loop to choose color
     let j;
     for (j = 0; j < rainbow.length; j++) {
       choosedColor = rainbow[Math.floor(7 * Math.random())];
     }
+    // custom event
     const event = new CustomEvent("sentence-choosed-color-changed", {
       detail: {
         newSentence: choosedSentence,
@@ -50,7 +57,7 @@
     });
     console.log(choosedSentence);
     console.log(choosedColor);
-    //conn && conn.close();
+    // checking if condition is true
     if (
       choosedSentence ===
         "Life is like a box of chocolates. You never know what you’re going to get. Forrest Gump" &&
@@ -58,10 +65,13 @@
     ) {
       condition = true;
     }
+    // condition is true and function is running (assignment with &&)
     condition && addTheBestText();
+    // dispatching event
     document.dispatchEvent(event);
   };
 
+  // event listener to set the quote on the page
   document.addEventListener(
     "sentence-choosed-color-changed",
     (sentenceEvent) => {
@@ -70,10 +80,12 @@
     }
   );
 
+  // event listener to set color of the header
   document.addEventListener("sentence-choosed-color-changed", (colorEvent) => {
     document.querySelector(".titleH1").style.backgroundColor =
       colorEvent.detail.newBackgroundColor;
   });
 
+  // event listener for the button
   sentenceButton.addEventListener("click", chooseSentenceAndChangeColor);
 })();
